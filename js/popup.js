bc835751ce592d2b914479b86c9144595354efeb
282Haniwa <282haniwa@gmail.com>
2019-09-04 16:18:22 +0900

    create option button click event.

--- js/popup.js
+++ js/popup.js
@@ -5,6 +5,7 @@
   const aliasList = JSON.parse(localStorage.getItem(aliasListKey))
   const searchFormElement = document.getElementById('search-form')
   const searchInputElement = document.getElementById('search-input')
+  const optionButton = document.getElementById('go-to-options')
 
   const convertSearchString = inputString => {
     let searchString = inputString
@@ -30,5 +31,14 @@
     window.open(buildURL(searchString), '_blank')
   }
 
+  const handleClickOptionButton = () => {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage()
+    } else {
+      window.open(chrome.runtime.getURL('options.html'))
+    }
+  }
+
   searchFormElement.addEventListener('submit', handleSearch)
+  optionButton.addEventListener('click', handleClickOptionButton)
 })()
